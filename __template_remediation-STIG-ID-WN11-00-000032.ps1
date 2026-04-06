5318c467a10b7f7fb69f78ed6b3b0877c5481c9a
Oliver Ponder <80069923+oponder2000@users.noreply.github.com>
2026-04-06 16:57:12 -0500

    Update __template_remediation-STIG-ID-WN11-00-000032.ps1

--- __template_remediation-STIG-ID-WN11-00-000032.ps1
+++ __template_remediation-STIG-ID-WN11-00-000032.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script ensures that a BitLocker PIN with a minimum length of six digits is used for pre-boot authentication.
 
 .NOTES
     Author          : Oliver Ponder
